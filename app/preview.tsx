157147c6af710667c0a60cb774c5617fdeffad4e
Nicolas Martinelli <nicolas@mintfull.agency>
2024-11-20 12:57:22 +0100

    Create app

---
+++ app/preview.tsx
@@ -0,0 +1,170 @@
+import React from "react";
+import { View, StyleSheet, Image, TouchableOpacity, Alert, Text } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { useLocalSearchParams, useGlobalSearchParams, useRouter } from "expo-router";
+import { Ionicons } from "@expo/vector-icons";
+import { PanGestureHandler, GestureHandlerRootView } from "react-native-gesture-handler";
+import Animated, { useSharedValue, useAnimatedStyle, withSpring } from "react-native-reanimated";
+
+export default function Preview() {
+  const insets = useSafeAreaInsets();
+  const glob = useGlobalSearchParams();
+  const local = useLocalSearchParams();
+  const router = useRouter();
+  const photoUri = glob.photoUri ? glob.photoUri : local.photoUri;
+
+  const translateY = useSharedValue(0);
+
+  if (!photoUri) {
+    return null;
+  }
+
+  const handleGesture = ({ translationY }: any) => {
+    translateY.value = translationY > 0 ? translationY : 0;
+  };
+
+  const handleGestureEnd = () => {
+    if (translateY.value > 300) {
+      router.push("/");
+    } else {
+      translateY.value = withSpring(0);
+    }
+  };
+
+  const animatedStyle = useAnimatedStyle(() => ({
+    transform: [{ translateY: translateY.value }],
+  }));
+
+  const handleFindInformation = async () => {
+    try {
+      const formData = new FormData();
+      formData.append("file", {
+        uri: photoUri, // URI de l'image
+        name: "image.jpg", // Nom du fichier
+        type: "image/jpeg", // Type MIME
+      });
+  
+      const response = await fetch("http://192.168.1.158:8000/recognize", {
+        method: "POST",
+        body: formData,
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      });
+  
+      if (!response.ok) {
+        throw new Error("Erreur lors de l'appel API");
+      }
+  
+      const result = await response.json(); // Résultat de l'API
+
+      console.log(result)
+  
+      // Redirection vers la page Result
+      router.push({
+        pathname: "/result",
+        params: {
+          character: result.character,
+          confidence: result.confidence.toString(),
+          image_url: result.image_url
+        },
+      });
+    } catch (error) {
+      console.error("Erreur lors de l'appel API :", error);
+      Alert.alert("Erreur", "Impossible de traiter l'image");
+    }
+  };
+
+  return (
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <PanGestureHandler
+        onGestureEvent={(event) => handleGesture(event.nativeEvent)}
+        onEnded={handleGestureEnd}
+      >
+        <Animated.View
+          style={[
+            styles.container,
+            animatedStyle,
+            { paddingTop: insets.top, paddingBottom: insets.bottom },
+          ]}
+        >
+          <Image
+            source={{ uri: photoUri }}
+            style={styles.image}
+            resizeMode="cover"
+          />
+          <View style={styles.buttonContainer}>
+            <TouchableOpacity style={styles.deleteButton} onPress={() => router.push("/")}>
+                <Ionicons name="add-outline" size={30} color="#fff" style={styles.deleteIcon} />
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.infoButton} onPress={handleFindInformation}>
+                <Text style={styles.infoText}>Find informations</Text>
+                <Ionicons name="sparkles-outline" size={20} color="#000" />
+            </TouchableOpacity>
+          </View>
+        </Animated.View>
+      </PanGestureHandler>
+    </GestureHandlerRootView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#000",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+    borderRadius: 30,
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    width: "90%",
+    position: "absolute",
+    bottom: 40,
+  },
+  deleteButton: {
+    width: 60,
+    height: 60,
+    borderRadius: 30,
+    borderWidth: 2,
+    borderColor: "#fff",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "transparent",
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 4 },
+    shadowOpacity: 0.3,
+    shadowRadius: 5,
+    elevation: 5,
+  },
+  deleteIcon: {
+    transform: [{ rotate: "45deg" }],
+  },
+  infoButton: {
+    flex: 1,
+    flexDirection: "row",
+    alignItems: "center",
+    paddingHorizontal: 20,
+    backgroundColor: "#fff",
+    borderRadius: 30,
+    height: 60,
+    justifyContent: "center",
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 4 },
+    shadowOpacity: 0.3,
+    shadowRadius: 5,
+    elevation: 5,
+    marginLeft: 20,
+  },
+  infoText: {
+    fontSize: 18,
+    fontWeight: 600,
+    marginRight: 20
+  }
+});
---
+++ app/result.tsx
@@ -0,0 +1,88 @@
+import React from "react";
+import { View, StyleSheet, Text, Image, TouchableOpacity } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { useLocalSearchParams, useRouter } from "expo-router";
+import { useFonts, Shrikhand_400Regular } from "@expo-google-fonts/shrikhand";
+import { Ionicons } from "@expo/vector-icons";
+
+export default function Result() {
+  const insets = useSafeAreaInsets();
+  const params = useLocalSearchParams();
+  const router = useRouter();
+  const { character, confidence, image_url } = params;
+
+  // Charger la police Shrikhand
+  const [fontsLoaded] = useFonts({
+    Shrikhand: Shrikhand_400Regular,
+  });
+
+  if (!fontsLoaded) {
+    return null; // Affichez un écran de chargement si nécessaire
+  }
+
+  console.log(`http://192.168.1.158:8000/${image_url}`)
+
+  return (
+    <View style={[styles.container, { paddingTop: insets.top }]}>
+      {image_url && (
+        <Image
+          source={{ uri: `http://192.168.1.158:8000/${image_url}` }}
+          style={styles.characterImage}
+          resizeMode="cover"
+        />
+      )}
+      <Text style={styles.characterText}>{character}</Text>
+      <Text style={styles.confidenceText}>
+        {`sure at ${(parseFloat(confidence) * 100).toFixed(2)}%`}
+      </Text>
+      <TouchableOpacity style={styles.closeButton} onPress={() => router.push("/")}>
+        <Ionicons name="add-outline" style={styles.closeIcon} size={40} color="#FFF" />
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#000",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  characterText: {
+    fontFamily: "Shrikhand", // Utilise la police Shrikhand
+    fontSize: 40,
+    color: "#fff",
+    marginBottom: 0,
+  },
+  confidenceText: {
+    fontSize: 16,
+    color: "#fff",
+  },
+  characterImage: {
+    width: 150, // Largeur de l'image
+    height: 150, // Hauteur de l'image
+    borderRadius: 30, // Bords arrondis
+    marginBottom: 20, // Espacement entre l'image et le texte
+  },
+  closeButton: {
+    width: 60,
+    height: 60,
+    borderRadius: 30,
+    borderWidth: 2,
+    borderColor: "#fff",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "transparent",
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 4 },
+    shadowOpacity: 0.3,
+    shadowRadius: 5,
+    elevation: 5,
+    position: "absolute",
+    bottom: 40,
+  },
+  closeIcon: {
+    transform: [{ rotate: "45deg" }],
+  }
+});
